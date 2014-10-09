7c6c4ce864e91374166f533e56bf81cc84ad8ef6
fpumir <fpumir@terredeweb.fr>
2014-10-09 16:43:12 +0200

    bubbling complete
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- video/js/video.js
+++ video/js/video.js
@@ -2,12 +2,21 @@
 
 var video=document.getElementById('video');
 var button=document.getElementById('button');
+var pB=document.getElementById('progressBar');
 button.classList.add('loading');
 video.load();
 
 video.addEventListener('canplaythrough',playPause,false);
 window.addEventListener('click',playPause,false);
 video.addEventListener('timeupdate',playProgress,false);
+pB.addEventListener('click',setVideoTime,false);
+
+function setVideoTime (e) {
+	e.stopPropagation();
+	console.log(e); 
+	video.currentTime=e.offsetX*video.duration/this.offsetWidth;
+}
+
 
 function playProgress () {
 	var self=this;
@@ -16,6 +25,11 @@ function playProgress () {
 }
 
 function playPause(e){
+	console.log(e); 
+	if(e.type=='canplaythrough'){
+		video.removeEventListener('canplaythrough',playPause,false);
+	}
+	
 	button.classList.remove('loading');
 	//var self=this;
 	if(video.paused){
